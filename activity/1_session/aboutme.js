4e67cfbec82b8af465fbc9d2991fb4240ade3a58
carito <dicalop7@gmail.com>
2022-01-11 15:46:53 -0500

    activity/1_session/aboutme.js is added bt alydev

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,14 @@
+let nombres = 'Jeysson Aly';
+let apellidos = 'Contreras Rodriguez';
+let profesion ='Ingeniero Electrónico';
+let targetprofile = 'Ingeniero DevOps';
+
+export function target() {
+    let aboutme = `Mi nombre es ${nombres} ${apellidos} , mi Profesión es ${profesion} estoy cursando este Master porque  quiero desempeñarme en  el puesto de ${targetprofile}`; 
+    console.log(aboutme)
+
+}
+target();
+
+
+
